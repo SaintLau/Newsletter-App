d7f393408f3467837f1c232e3fe8cfc7c528bbb2
Laura Santos <alsr16p@gmail.com>
2021-10-09 18:39:54 +0100

    set mailchimp api to receive data

--- app.js
+++ app.js
@@ -15,14 +15,52 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-    let firstName = req.body.fName;
-    let lastName = req.body.lName;
-    let email = req.body.email;
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
 
-    console.log(firstName, lastName, email);
+    //set object to sent data to MailChimp using keys they will recognise
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    //they need to receive data in a string format, so we'll use stringify to have a JSON
+    const jsonData = JSON.stringify(data); //-> what will be send to MailChimp
+
+    const url = "https://us5.api.mailchimp.com/3.0/lists/192b6d7ef0";
+
+    const options = {
+        method: "POST",
+        auth: "lauras:6bb505f3ef5685012fb0ba1f8eae9112-us5" //-> mailchimp docs saythe user name is what we want but the pass is the key 
+
+    }
+
+    const request = https.request(url, options, function(response) {
+       response.on("data", function(data) {
+           console.log(JSON.parse(data));
+       })
+    })
+    request.write(jsonData);
+    request.end();
 });
 
 
 app.listen(port, () => {
     console.log(`Server listenning at ${port}`);
 });
+
+
+//API Key - MAilChimp
+//6bb505f3ef5685012fb0ba1f8eae9112-us5
+
+//Audience ID 
+//192b6d7ef0
