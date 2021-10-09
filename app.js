1845fc1453bce56b42a29212b0969b5f77e30a8c
Laura Santos <alsr16p@gmail.com>
2021-10-09 17:11:03 +0100

    server ready to send data to api

--- app.js
+++ app.js
@@ -7,9 +7,21 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(express.static("public"));
+
+//for signup page 
 app.get('/', (req, res) => {
-    res.send("Hello World!");
+    res.sendFile(__dirname + '/signup.html');
 });
+
+app.post('/', (req, res) => {
+    let firstName = req.body.fName;
+    let lastName = req.body.lName;
+    let email = req.body.email;
+
+    console.log(firstName, lastName, email);
+});
+
 
 app.listen(port, () => {
     console.log(`Server listenning at ${port}`);
